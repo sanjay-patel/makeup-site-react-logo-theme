92cb94af47ee10c4270a16dc5d43fbe6ad49f0f5
Sanjay Patoliya <sanjay.patoliya@teamwass.com>
2025-11-23 21:15:41 +0530

    Add redirect from Amplify URL to custom domain for SEO

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '.*\\.amplifyapp\\.com',
+          },
+        ],
+        destination: 'https://bhumimakeupartistry.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
